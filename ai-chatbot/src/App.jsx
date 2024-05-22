4367e8be93379102599dcd37d9b3d8d998a4f4d2
chimsom <okorogladys52@gmail.com>
2024-05-22 13:19:56 +0100

    create router

---
+++ ai-chatbot/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+<Container className='mt-3'>
+  
+</Container>
+);
+}
+
+export default App
